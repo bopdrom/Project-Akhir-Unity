463ee27a3c86361597ba3e46a67993430b32bd88
HafidzMSN <135082917+HafidzMSN@users.noreply.github.com>
2024-05-02 19:37:09 +0700

    update

---
+++ Assets/Scripts/Popup/Pause.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject objectToToggle;
+    public string level = "";
+
+    void Update(){
+        if(Input.GetKeyDown(KeyCode.Escape)){
+            pauseMenu();
+        }
+    }
+
+    public void pauseMenu(){
+		objectToToggle.SetActive(true);
+		Time.timeScale = 0f;
+	}
+
+    public void pause(){
+		objectToToggle.SetActive(true);
+		Time.timeScale = 0f;
+	}
+
+	public void Resume(){
+		objectToToggle.SetActive(false);
+		Time.timeScale = 1f;
+	}
+
+    public void restart(){
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(level);
+    }
+}
